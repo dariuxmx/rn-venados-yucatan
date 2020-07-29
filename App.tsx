cf908a7586cd98b4068aee9f7ea02c97475184f2
dariuxmx <dario@habanerostudio.mx>
2020-07-28 21:34:20 -0500

    Initial setup.

--- App.tsx
+++ App.tsx
@@ -1,13 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { ThemeProvider } from "styled-components/native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      {/* <Button
+        title="Go to Statistics"
+        onPress={() => navigation.navigate('Statistics')}
+      /> */}
+    </View>
+  );
+}
+
+function StatisticsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Statistics Screen</Text>
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} 
+          options={{
+            title: "Venados F.C. Yucatán",
+            headerTintColor: "green",
+          }}
+        />
+        <Drawer.Screen name="Statistics" component={StatisticsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+    
   );
 }
 
